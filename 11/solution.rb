caf538412ea4257434dbd68cfdbec4faff65acdd
Donald Hutchison <git@toastymofo.net>
2018-12-16 14:32:08 +0100

    day 11 sat implementation whoa.

--- 11/solution.rb
+++ 11/solution.rb
@@ -1,46 +1,69 @@
-power_grid = Array.new(300){ Array.new(300) }
-total_power = Array.new(300){ Array.new(300) }
+$sat = Array.new(300){ Array.new(300,0) }
 
 $input = 5034
+
 def power_for_coord x,y,serial
   rack = x + 10
   power = rack * y
   power += serial
   power *= rack
   ((power / 100) % 10) - 5
-
 end
 
-
-def neighbours x,y
-  [x-1,y-1,x,y-1,x+1,y-1,
-  x-1,y,x,y,x+1,y,
-  x-1,y+1,x,y+1,x+1,y+1]
-end
-
-def cost x,y,power
-  cells = neighbours x, y
-  cells.each_slice(2).to_a.map { |c| power.dig(*c) }.sum
-end
-
-300.times do |x|
-  300.times do |y|
-    power_grid[x][y] = power_for_coord x, y, $input
-  end
-end
-
-1.upto(298) do |x|
-  1.upto(298) do |y|
-    total_power[x][y] = cost(x,y,power_grid)
-  end
-end
-
-max = total_power.flatten.compact.max
-total_power.each_with_index do |c,x|
-  c.each_with_index do |v,y|
-    if v == max
-      puts "#{x-1}, #{y-1}"
-      break
+def iterate_grid(max=299)
+  0.upto(max) do |y|
+    0.upto(max) do |x|
+      yield(x,y)
     end
   end
 end
+
+
+def sat_value x,y,grid
+  a = y-1 >= 0 ? grid[x][y-1] : 0
+  b = x-1 >= 0 ? grid[x-1][y] : 0
+  c = (x-1 >= 0 and y-1 >= 0) ?  grid[x-1][y-1] : 0
+
+  a + b - c
+end
+
+#populate summed area table
+iterate_grid { |x,y| $sat[x][y] = power_for_coord(x, y, $input)+sat_value(x,y,$sat)}
+
+def access x,y,grid
+  return grid[x][y] if x >= 0 and y >= 0
+  0
+end
+
+def sat_cost x, y, size,sat=$sat
+  access(x+size,y+size,sat) + access(x-1,y-1,sat) - access(x+size,y-1,sat) - access(x-1,y+size,sat)
+end
+
+#find max 3x3 area
+max = [0,0,0]
+iterate_grid do |x,y|
+  next if x + 2 >= 299 or y + 2 >= 299
+  if sat_cost(x,y,2) > max[2]
+    max = [x,y, sat_cost(x,y,2) ]
+  end
+end
+
+puts max.join(",")
+
+#part 2
+
+#find max nxn area
+result = [nil,nil,0,0]
+1.upto(298) do |x|
+  1.upto(298) do |y|
+    1.upto(17).each do |size|
+      next if x+size >=300 or y+size >= 300
+      if sat_cost(x,y,size) > result[3]
+        result = [x,y,size,sat_cost(x,y,size) ]
+      end
+    end
+  end
+end
+
+x,y,size,_ = result
+puts "#{x},#{y},#{size+1}"

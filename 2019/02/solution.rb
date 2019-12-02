1109bbc39ffcf381fba62fe0edac9c2e8c9f1a52
Donald Hutchison <git@toastymofo.net>
2019-12-02 12:03:38 +0100

    day 2 baby

--- 2019/02/solution.rb
+++ 2019/02/solution.rb
@@ -1,1 +1,40 @@
 input = File.open("input").read.split(",").map(&:to_i)
+
+@opcodes = {
+  1 => ->(arr, i){  arr[arr[i+3]] = arr[arr[i+1]] + arr[arr[i+2]] },
+  2 => ->(arr, i){  arr[arr[i+3]] = arr[arr[i+1]] * arr[arr[i+2]] },
+  99 => ->(arr, i){ "noop" },
+}
+
+def run inp
+  position = 0
+
+  loop do
+    value = inp[position]
+    @opcodes[value].(inp,position)
+
+    break if value == 99
+    position += 4
+  end
+
+  inp[0]
+end
+
+part1 = input.clone
+part1[1] = 12
+part1[2] = 2
+puts run(part1)
+
+(0..99).each do |noun|
+  (0..99).each do |verb|
+    part2 = input.clone
+    part2[1] = noun
+    part2[2] = verb
+    result = run(part2)
+
+    if result == 19690720
+      puts noun * 100 + verb
+      exit
+    end
+  end
+end

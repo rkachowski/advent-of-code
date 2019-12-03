323a739bb423ad034f6be41295d46fe16f03c075
Donald Hutchison <git@toastymofo.net>
2019-12-03 15:46:38 +0100

    3 part 2.

--- 2019/03/solution.rb
+++ 2019/03/solution.rb
@@ -1,12 +1,3 @@
-require 'pry-byebug'
-
-test = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
-U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
-
-test = "R75,D30,R83,U83,L12,D49,R71,U7,L72
-U62,R66,U55,R34,D71,R55,D58,R83"
-
-input = test.lines.map{|l| l.split(",")}
 input = File.open("input").read.lines.map{|l| l.split(",")}
 
 grid = Array.new(20000){ Array.new(20000) }
@@ -32,25 +23,19 @@ end
 def draw_line line, grid, id
   position = @center.clone
 
+  steps = 0
   line.each do |ex|
-    steps = 0
     dir, mag, _ = ex.partition(/\d+/)
 
     dir = @dirs[dir]
 
     0.upto(mag.to_i - 1) do
-      begin
       grid[position[0]][position[1]] ||=[]
       grid[position[0]][position[1]] << [id, steps]
+      steps += 1
 
       position[0] += dir[0]
       position[1] += dir[1]
-      steps += 1
-      rescue Exception => e
-        puts position
-        puts ex
-        exit
-      end
     end
   end
 end
@@ -60,8 +45,14 @@ input.each_with_index do |line, i|
 end
 
 iterate_grid(grid) do |x,y,b|
-  if b && b.uniq.size > 1
-    puts "crossing - #{x - @center[0]}, #{y - @center[1]} - #{(x - @center[0]).abs + (y - @center[1]).abs}"
+  if b 
+    lines = b.map(&:first)
+    if lines.uniq.size > 1
+      l1, l2 = b.select{|l| l.first == 0}.map(&:last), b.select{|l| l.first == 1}.map(&:last)
+
+      min_steps = l1.min + l2.min
+      puts "crossing - #{x - @center[0]}, #{y - @center[1]} - #{(x - @center[0]).abs + (y - @center[1]).abs} - min_steps = #{min_steps}"
+    end
   end
 end
 

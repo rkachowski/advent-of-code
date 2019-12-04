f806db3f7ee7aa70b1ae92647c83d5c15a90fc33
Donald Hutchison <git@toastymofo.net>
2019-12-04 15:48:31 +0100

    whatever.

--- 2019/04/solution.rb
+++ 2019/04/solution.rb
@@ -1,7 +1,7 @@
 input = 145852..616942
 
-seq = ->(arr,i){ i < arr.size - 1 and arr[i+1] == arr[i] }
-not_dec = ->(arr,i){ i == arr.size - 1 or arr[i+1] >= arr[i]}
+seq = ->(digits){ digits.each_with_index.any? {|_,i| i < digits.size - 1 and digits[i+1] == digits[i] } }
+ascending = ->(digits){digits.inject(0) { |p,d| return false if p > d; d }; true  }
 not3 = ->(digits){
   acc = digits.each_with_index.inject({}) do |h,(d,i)|
     h[d] ||= 0
@@ -14,13 +14,12 @@ not3 = ->(digits){
 
 valid1 = ->(num) {
   digits = num.digits.reverse
-  (digits.each_with_index.any? {|_, n| seq.(digits, n)}) and (digits.each_with_index.all? {|_,n| not_dec.(digits, n)})
+  seq.(digits) and ascending.(digits)
 }
 valid2 = ->(num) {
   digits = num.digits.reverse
-  not3.(digits)  and (digits.each_with_index.all? {|_,n| not_dec.(digits, n)})
+  not3.(digits)  and ascending.(digits)
 }
-
 
 puts input.each.count {|n| valid1.(n)}
 puts input.each.count {|n| valid2.(n)}

c9bc5e7e32bd4e1d2593069c424d0d22fad57af5
Donald Hutchison <git@toastymofo.net>
2019-12-07 20:10:32 +0100

    Day 5 step one whoa.

--- 2019/05/solution.rb
+++ 2019/05/solution.rb
@@ -4,10 +4,9 @@ input = File.open("input").read.chomp.split(",").map(&:to_i)
 @input = []
 @output = []
 
-@position = ->(arr,i){ 
-  arr[arr[i]]
-}
+@position = ->(arr,i){ arr[arr[i]] }
 @immediate = ->(arr,i){ arr[i] }
+
 @modes = {
   0 => @position,
   1 => @immediate
@@ -18,6 +17,10 @@ input = File.open("input").read.chomp.split(",").map(&:to_i)
   2 => ->(arr, i,mode){  arr[arr[i+3]] = mode[0].(arr,i+1) * mode[1].(arr,i+2); 4},
   3 => ->(arr, i,mode){  arr[arr[i+1]] = @input.pop; 2},
   4 => ->(arr, i,mode){  @output.push(mode[0].(arr, i+1 )); 2},
+  5 => ->(arr, i,mode){ mode[0].(arr, i+1) != 0 ? mode[1].(arr, i+2) - i : 3 },
+  6 => ->(arr, i,mode){ mode[0].(arr, i+1) == 0 ? mode[1].(arr, i+2) - i : 3 },
+  7 => ->(arr, i,mode){ arr[arr[i+3]] = (mode[0].(arr, i+1) < mode[1].(arr, i+2)  ? 1 : 0); 4 },
+  8 => ->(arr, i,mode){ arr[arr[i+3]] = (mode[0].(arr, i+1) == mode[1].(arr, i+2)  ? 1 : 0); 4 },
   99 => ->(arr, i, _){ @exit = true; 1 }
 }
 
@@ -56,6 +59,12 @@ def run inp
 end
 
 @input.push 1
+run input
+
+puts @output.inspect
+
+@input = [5]
+@output = []
 run input
 
 puts @output.inspect

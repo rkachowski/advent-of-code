faff05d4c946efeac04f4c9924d09efbe473788e
Donald Hutchison <git@toastymofo.net>
2019-12-07 20:03:30 +0100

    Day 5 step one whoa.

---
+++ 2019/05/solution.rb
@@ -0,0 +1,61 @@
+input = File.open("input").read.chomp.split(",").map(&:to_i)
+
+@exit = false
+@input = []
+@output = []
+
+@position = ->(arr,i){ 
+  arr[arr[i]]
+}
+@immediate = ->(arr,i){ arr[i] }
+@modes = {
+  0 => @position,
+  1 => @immediate
+}
+
+@opcodes = {
+  1 => ->(arr, i,mode){  arr[arr[i+3]] = mode[0].(arr,i+1) + mode[1].(arr,i+2); 4},
+  2 => ->(arr, i,mode){  arr[arr[i+3]] = mode[0].(arr,i+1) * mode[1].(arr,i+2); 4},
+  3 => ->(arr, i,mode){  arr[arr[i+1]] = @input.pop; 2},
+  4 => ->(arr, i,mode){  @output.push(mode[0].(arr, i+1 )); 2},
+  99 => ->(arr, i, _){ @exit = true; 1 }
+}
+
+
+def get_modes value
+  modes = (value / 100).to_s.chars
+
+  loop do
+    if modes.size < 3
+      modes.unshift "0"
+    else
+      break
+    end
+  end
+
+  modes.reverse.map {|f| @modes[f.to_i]}
+end
+
+def run inp
+  position = 0
+
+  loop do
+    value = inp[position]
+
+    opcode = value % 100
+    func = @opcodes[opcode]
+
+    modes = get_modes(value)
+    ip_inc = func.(inp,position, modes)
+
+    break if @exit
+    position += ip_inc
+  end
+
+  inp[0]
+end
+
+@input.push 1
+run input
+
+puts @output.inspect

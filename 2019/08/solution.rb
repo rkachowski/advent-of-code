aa26e801aed0802d429eba607d81fada73619180
Donald Hutchison <git@toastymofo.net>
2019-12-09 11:01:23 +0100

    day 8 pt 2.

--- 2019/08/solution.rb
+++ 2019/08/solution.rb
@@ -5,8 +5,15 @@ width, height = 25, 6
 
 layers = input.each_slice(width * height).map{ |s| s.each_slice(width).to_a }
 min_layer = layers.min {|l,l2| l.flatten.count("0") <=> l2.flatten.count("0")}
-
-binding.pry
 puts min_layer.flatten.count("1") * min_layer.flatten.count("2")
 
-#replace 2 with nil, 1 with white and 0 with black
+first = layers.pop
+layers.reverse.each do |cur_layer|
+  cur_layer.each_with_index do |row, row_number|
+    row.each_with_index do |pix,i|
+      first[row_number][i] = pix if pix == "1" or pix == "0"
+    end
+  end
+end
+
+puts first.map {|l|l.join.gsub('0','.')}

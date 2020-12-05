525be2cfe4c48663ff1c9929402fd0b4e3c81a87
Donald Hutchison <git@toastymofo.net>
2020-12-05 15:36:33 +0100

    Formatting

--- 2020/5/solution.exs
+++ 2020/5/solution.exs
@@ -1,44 +1,48 @@
 defmodule Day5 do
   def parse do
     {_, file} = File.read("input")
+
     file
     |> String.split("\n", trim: true)
   end
 
   def solve do
-      input = parse()
+    input = parse()
 
-      seats = input
+    seats =
+      input
       |> Enum.map(&seat/1)
 
-      part1 = seats
+    part1 =
+      seats
       |> Enum.map(fn {row, column} -> row * 8 + column end)
-      |> Enum.max
+      |> Enum.max()
 
-      IO.puts part1
+    IO.puts(part1)
 
-      {row, taken} = seats
-      |> Enum.reduce(%{}, fn {row, column}, acc -> Map.put(acc, row, [column | acc[row] || [] ] ) end)
-      |> Map.new(fn {k,v} -> {k, Enum.sort(v)} end)
-      |> Enum.find(fn {_,v} -> length(v) == 7 end)
+    {row, taken} =
+      seats
+      |> Enum.reduce(%{}, fn {row, column}, acc ->
+        Map.put(acc, row, [column | acc[row] || []])
+      end)
+      |> Map.new(fn {k, v} -> {k, Enum.sort(v)} end)
+      |> Enum.find(fn {_, v} -> length(v) == 7 end)
 
-      [column] = (0..7 |> Enum.to_list) -- taken
+    [column] = (0..7 |> Enum.to_list()) -- taken
 
-      IO.puts row * 8 + column
+    IO.puts(row * 8 + column)
   end
 
   def seat(input) do
-    {row, column} =  input |> String.graphemes |> Enum.split(7)
-    {seat(row, 0..127 |> Enum.to_list), seat(column, 0..7 |> Enum.to_list)}
+    {row, column} = input |> String.graphemes() |> Enum.split(7)
+    {seat(row, 0..127 |> Enum.to_list()), seat(column, 0..7 |> Enum.to_list())}
   end
 
-  def seat([h | t], r) do
-    {lower, upper} = Enum.split(r, div(length(r),2))
+  def seat([dir | t], r) when dir == "F" or dir == "L",
+    do: seat(t, Enum.split(r, div(length(r), 2)) |> elem(0))
 
-    if h == "F" or h == "L", do: seat(t, lower), else: seat(t, upper)
-  end
-
-  def seat([], [h | _ ]), do: h
+  def seat([_ | t], r), do: seat(t, Enum.split(r, div(length(r), 2)) |> elem(1))
+  def seat([], [h | _]), do: h
 end
 
-Day5.solve
+Day5.solve()

bb9644e789c83c303cdeb813d173b9de6d657c47
Donald Hutchison <git@toastymofo.net>
2020-12-06 12:50:06 +0100

    yeah whatever

--- 2020/6/solution.exs
+++ 2020/6/solution.exs
@@ -16,8 +16,7 @@ defmodule Day6 do
       end)
 
     unique_answers_in_group
-    |> Enum.map(&Enum.count/1)
-    |> Enum.sum()
+    |> Enum.reduce(0, &(Enum.count(&1) + &2))
     |> IO.puts()
 
     groups =
@@ -25,13 +24,12 @@ defmodule Day6 do
       |> Enum.map(&Enum.map(&1, fn g -> String.graphemes(g) |> MapSet.new() end))
 
     unique_answers_in_group
-      |> Enum.zip(groups)
-      |> Enum.map(fn {map, groups} ->
-        Enum.count(map, fn c -> Enum.all?(groups, &MapSet.member?(&1, c)) end)
-      end)
-      |> Enum.sum
-      |> IO.puts
-
+    |> Enum.zip(groups)
+    |> Enum.map(fn {map, groups} ->
+      Enum.count(map, fn c -> Enum.all?(groups, &MapSet.member?(&1, c)) end)
+    end)
+    |> Enum.sum()
+    |> IO.puts()
   end
 end
 

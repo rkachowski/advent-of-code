da6818fcf44bf9a71d3ca3e104e07104f92b6377
Donald Hutchison <git@toastymofo.net>
2020-12-27 23:57:15 +0100

    Day 23 real.

--- 2020/23/solution.exs
+++ 2020/23/solution.exs
@@ -6,87 +6,87 @@ defmodule Day23 do
       "input" -> "925176834"
       "test" -> "389125467"
     end
-    |> String.graphemes
+    |> String.graphemes()
     |> Enum.map(&String.to_integer/1)
   end
 
   def solve do
     input = parse()
 
-    part1(input)
+    input |> part1
+    input |> part2
+  end
 
-    part2(input)
+  def ring_map(list) do
+    last = List.last(list)
+    Enum.reduce(list, {%{}, last}, fn i, {m, l} -> {Map.put(m, l, i), i} end) |> elem(0)
+  end
+
+  def output(map) do
+    2..8
+    |> Enum.reduce({[map[1]], map[1]}, fn _, {l, i} -> {[map[i] | l], map[i]} end)
+    |> elem(0)
+    |> Enum.reverse()
+    |> Enum.join()
+  end
+
+  def take(map, current) do
+    a = map[current]
+    b = map[a]
+    c = map[b]
+
+    {Map.put(map, current, map[c]), [a, b, c]}
+  end
+
+  def put(map, [h, _, t], destination) do
+    c = map[destination]
+
+    map
+    |> Map.put(destination, h)
+    |> Map.put(t, c)
   end
 
   def part1(input) do
+    current = hd(input)
+    m = ring_map(input)
+    max = Enum.max(input)
+
     1..100
-    |> Enum.reduce({0, input}, fn _, {i,l} -> move(l,i) end)
-    |> elem(1)
-    |> order
-    |> IO.puts
+    |> Enum.reduce({m, current}, fn _, {map, current} -> step(map, current, max) end)
+    |> elem(0)
+    |> output
+    |> IO.puts()
   end
 
   def part2(input) do
-    input = input ++ Enum.to_list(10..1000000)
+    c = hd(input)
 
-    1..10000000
-    |> Enum.reduce({0, input}, fn t, {i,l} ->
-      IO.puts t
-      move(l,i)
-    end)
-    |> elem(1)
-    |> find_stars
-    |> IO.puts
+    m = ring_map(input ++ Enum.to_list(10..1_000_000))
+    max = m |> Map.values() |> Enum.max()
+
+    m =
+      1..10_000000
+      |> Enum.reduce({m, c}, fn _, {map, current} -> step(map, current, max) end)
+      |> elem(0)
+
+    r = m[1]
+    r2 = m[r]
+    IO.puts(r * r2)
   end
 
-  def find_stars l do
-    IEx.pry
-    one_index = Enum.find_index(l, &(&1 == 1))
-    [Enum.at(l,one_index + 1),Enum.at(l,one_index + 2)]
-  end
-  def order(l) do
-    one_index = Enum.find_index(l, &(&1 == 1))
-    {h, [_ |t]} = Enum.split(l, one_index)
-    Enum.join(t ++ h)
+  def step(map, current, max) do
+    {map, to_take} = take(map, current)
+    d = find_destination(current - 1, to_take, max)
+    {put(map, to_take, d), map[current]}
   end
 
-  def move(l, i) when i == length(l), do: move(l, 0)
+  def find_destination(0, to_take, max), do: find_destination(max, to_take, max)
 
-  def move(l, i) do
-    current = Enum.at(l, i)
-    {l, taken} = take_3(l, i)
-
-    destination = find_destination(current - 1, l)
-
-    destination_index = Enum.find_index(l, &(&1 == destination))
-
-    {h,t} = Enum.split(l,destination_index + 1)
-    result = h ++ taken ++ t
-
-    {Enum.find_index(result, &(&1 == current)) + 1, result}
-  end
-
-  def take_3(l, i) when i < length(l) - 3 do
-    {h, t} = Enum.split(l, i + 1)
-    {take, t} = Enum.split(t, 3)
-    {h ++ t, take}
-  end
-
-  def take_3(l = [h| t], i) when i == length(l) - 3 do
-    {e, t} = Enum.split(t, 6)
-    {e, t ++ [h]}
-  end
-
-  def take_3(l = [h | [n | t]], i) when i == length(l) - 2 do
-    {e, t} = List.pop_at(t, -1)
-    {t, [e,h,n]}
-  end
-
-  def find_destination(0, add_into), do: Enum.max(add_into)
-
-  def find_destination(current, add_into) do
-    if Enum.member?(add_into, current), do: current, else: find_destination(current - 1, add_into)
+  def find_destination(current, to_take, max) do
+    if Enum.member?(to_take, current),
+      do: find_destination(current - 1, to_take, max),
+      else: current
   end
 end
 
-Day23.solve
+Day23.solve()

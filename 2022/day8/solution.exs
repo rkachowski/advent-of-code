a8c3d7217da3b02793df454da6798896dd23f1a8
Donald Hutchison <git@toastymofo.net>
2022-12-09 19:22:44 +0100

    Day 8 2022 pt1.

--- 2022/day8/solution.exs
+++ 2022/day8/solution.exs
@@ -3,25 +3,60 @@ Code.require_file("grid.exs")
 defmodule Main do
   def run() do
     grid =
-      %{height: height} =
-      "test"
+      %{height: height, width: width} =
+      "input"
       |> File.read!()
       |> Grid.new()
       |> Grid.map(fn {k, v} -> {k, String.to_integer(v)} end)
 
-    result =
+    rows =
       1..(height - 1)
       |> Enum.flat_map(&visible_row(grid, &1))
       |> MapSet.new()
+
+    columns =
+      1..(width - 1)
+      |> Enum.flat_map(&visible_column(grid, &1))
+      |> MapSet.new()
+
+    result =
+      MapSet.union(rows, columns)
+      |> MapSet.union(edge_coords(grid))
 
     require IEx
     IEx.pry()
   end
 
-  def visible_row(grid, row) do
-    0..(grid.width - 1)
-    |> Enum.map(&{&1, row})
-    |> visible(grid)
+  def visible_row(grid = %{height: h, width: w}, row) do
+    left =
+      [{0, row}]
+      |> gen_coord(fn {x, y} -> {x + 1, y} end, {w, h})
+      |> visible(grid)
+      |> MapSet.new()
+
+    right =
+      [{w - 1, row}]
+      |> gen_coord(fn {x, y} -> {x - 1, y} end, {w, h})
+      |> visible(grid)
+      |> MapSet.new()
+
+    MapSet.union(left, right)
+  end
+
+  def visible_column(grid = %{height: h, width: w}, column) do
+    top =
+      [{column, 0}]
+      |> gen_coord(fn {x, y} -> {x, y + 1} end, {w, h})
+      |> visible(grid)
+      |> MapSet.new()
+
+    bottom =
+      [{column, h - 1}]
+      |> gen_coord(fn {x, y} -> {x, y - 1} end, {w, h})
+      |> visible(grid)
+      |> MapSet.new()
+
+    MapSet.union(top, bottom)
   end
 
   def visible(coords = [first | _], grid) do
@@ -39,11 +74,28 @@ defmodule Main do
     visible_cells
   end
 
-  
+  def gen_coord(coords = [hd = {x, y} | _], func, {max_x, max_y} = max)
+      when x >= 0 and y >= 0 and x < max_x and y < max_y do
+    gen_coord([func.(hd) | coords], func, max)
+  end
+
+  def gen_coord([_ | coords], _func, _max), do: Enum.reverse(coords)
+
+  def edge_coords(%{width: w, height: h}) do
+    top_and_bottom =
+      for x <- 0..(w - 1), reduce: [] do
+        acc ->
+          [{x, 0}, {x, h - 1}] ++ acc
+      end
+
+    left_and_right =
+      for y <- 0..(h - 1), reduce: [] do
+        acc ->
+          [{0, y}, {w - 1, y}] ++ acc
+      end
+
+    MapSet.new(top_and_bottom ++ left_and_right)
+  end
 end
 
 Main.run()
-
-
-fn coord = {x,y}, acc when x >= 0 and y >= 0 ->
-  {:cont, [ func.(coord) | acc]}

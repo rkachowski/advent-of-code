430d59217aa9f3a08db194c0e68e65cf0683eacf
Donald Hutchison <git@toastymofo.net>
2024-12-05 15:50:10 +0100

    Parse file util.

---
+++ 2024/utils.go
@@ -0,0 +1,19 @@
+package utils
+
+func ParseFile(filename string) []string {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
+
+
+

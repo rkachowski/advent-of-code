6edf6022cdef43083b21212bd850d6f452fa6a96
Donald Hutchison <git@toastymofo.net>
2024-12-07 10:03:51 +0100

    Day 3.

--- 2024/3/main.go
+++ 2024/3/main.go
@@ -6,6 +6,7 @@ import (
 	"regexp"
 	"slices"
 	"strconv"
+	"strings"
 )
 
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
@@ -14,21 +15,54 @@ import (
 func main() {
 	input := utils.ParseFile("input")
 
+	flattened := strings.Join(input, "")
+	total := extractMuls(flattened)
+	log.Println(total)
+
+	matches := extractEverything(flattened)
+	var validMuls [][]string
+	enabled := true
+	for _, m := range matches {
+		if m[0] == "don't()" {
+			enabled = false
+		} else if m[0] == "do()" {
+			enabled = true
+		} else if enabled == true {
+			validMuls = append(validMuls, m)
+		}
+	}
+
+	part2 := sumMuls(validMuls)
+
+	log.Println(part2)
+}
+
+func extractMuls(input string) int {
 	pattern := `mul\((\d+),(\d+)\)`
 
 	re := regexp.MustCompile(pattern)
 	var matches [][]string
-	for _, line := range input {
-		muls := re.FindAllStringSubmatch(line, -1)
-		matches = slices.Concat(matches, muls)
-	}
+	muls := re.FindAllStringSubmatch(input, -1)
+	matches = slices.Concat(matches, muls)
 
+	return sumMuls(matches)
+}
+
+func sumMuls(matches [][]string) int {
 	total := 0
-
 	for _, m := range matches {
 		i, _ := strconv.Atoi(m[1])
 		j, _ := strconv.Atoi(m[2])
 		total += i * j
 	}
-	log.Println(total)
+	return total
+}
+
+func extractEverything(input string) [][]string {
+	pattern := `mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)`
+
+	re := regexp.MustCompile(pattern)
+	matches := re.FindAllStringSubmatch(input, -1)
+
+	return matches
 }
